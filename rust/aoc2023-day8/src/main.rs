1cfaebcea61bb736452ee0854b2d2b3f8de7b75c
Alek Gajos <alek.gajos@gmail.com>
2024-04-04 22:15:32 +0200

    Day 8 tree attempt

--- rust/aoc2023-day8/src/main.rs
+++ rust/aoc2023-day8/src/main.rs
@@ -1,5 +1,5 @@
 use regex::Regex;
-use std::cell::OnceCell;
+use std::cell::RefCell;
 use std::fmt;
 use std::fs::File;
 use std::io::{self, BufRead, BufReader};
@@ -16,7 +16,7 @@ pub enum ParsingError {
     IOError(#[from] io::Error),
 }
 
-type NodeRef = Rc<OnceCell<TreeNode>>;
+type NodeRef = Option<Rc<RefCell<TreeNode>>>;
 
 struct TreeNode {
     label: String,
@@ -30,78 +30,75 @@ struct Problem {
     instructions: Instructions,
 }
 
-// fn get_node(label: &str, tree: &TreeNode) -> NodeRef {
-//
-//     let node = dict.get(label);
-//     let node = TreeNode::find_or_create(root, label)
-//
-//     if let Some(node) = node {
-//         let left = dict.get()
-//
-//         node.left.get_or_init(|| get_node(left, dict).);
-//
-//        Rc::from(TreeNode {
-//                 label: label.to_owned(),
-//                 left:
-//                 right: get_node(right, dict),
-//             })
-//
-//         if label != left && label != right {
-//             Some()
-//         } else {
-//             None
-//         }
-//     } else {
-//         None
-//     }
-// }
-
 impl fmt::Debug for Problem {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-       TreeNode::print_node(&self.root, f)
+        TreeNode::print_node(&self.root, f)
     }
 }
 
 impl TreeNode {
     pub fn print_node(noderef: &NodeRef, f: &mut fmt::Formatter) -> fmt::Result {
-
-        if let Some(node) = noderef.get() {
-            let _ = TreeNode::print_node(&node.left, f);
-            let res = write!(f, "{} ", node.label);
-            let _ = TreeNode::print_node(&node.right, f);
-            res 
-        }else{
+        if let Some(node) = noderef {
+            println!("Calling recursive print_node");
+            let _ = TreeNode::print_node(&node.borrow().left, f);
+            let res = write!(f, "{} ", node.borrow().label);
+            let _ = TreeNode::print_node(&node.borrow().right, f);
+            res
+        } else {
+            println!("Called print_node with an empty noderef");
             Ok(())
         }
     }
 
-    pub fn find(noderef: &NodeRef, label: &str) -> Option<NodeRef> {
-        if let Some(node) = noderef.get() {
+    pub fn find(noderef: &NodeRef, label: &str) -> NodeRef {
+        println!("FIND: entering function for {}", label);
+
+        if let Some(node) = noderef {
+
+            let node = node.borrow();
+
+            println!(
+                "FIND: node.label={}, label param={}",
+                &node.label,
+                label
+            );
+
             if node.label == label {
-                Some(noderef.clone())
+                println!("FIND: found match {}", &node.label);
+                noderef.clone()
+            } else if node.label == "ZZZ" {
+                None
             } else {
-                if let Some(node) = TreeNode::find(&node.left, label) {
-                    Some(noderef.clone())
-                } else {
-                    TreeNode::find(&node.right, label)
+                let left_find = TreeNode::find(&node.left, label);
+                let right_find = TreeNode::find(&node.right, label);
+                if left_find.is_some() {
+                    left_find.clone()
+                }else if right_find.is_some() {
+                    right_find
+                }else{
+                    None
                 }
             }
+
         } else {
             None
         }
     }
 
     pub fn find_or_create(root: &NodeRef, label: &str) -> NodeRef {
+        println!("find_or_create called");
         match TreeNode::find(root, label) {
-            Some(node) => node,
-            None => Rc::new(
-                TreeNode {
-                    label: label.to_owned(),
-                    left: Rc::new(OnceCell::new()),
-                    right: Rc::new(OnceCell::new()),
-                }
-                .into(),
-            ),
+            Some(node) => Some(node),
+            None => {
+                println!("Creating new node {}", &label);
+                Some(Rc::new(RefCell::new(
+                    TreeNode {
+                        label: label.to_owned(),
+                        left: None,
+                        right: None,
+                    }
+                )))
+            }
         }
     }
 }
@@ -150,8 +147,8 @@ fn parse(file_path: &str, part_two: bool) -> Result<Problem, ParsingError> {
 
     // let mut dict = HashMap::new();
 
-    let root: NodeRef = Rc::new(OnceCell::new());
-    let root = TreeNode::find_or_create(&root, "AAA");
+    let root: NodeRef = None;
+    let root: NodeRef = TreeNode::find_or_create(&root, "AAA");
 
     reader.lines().for_each(|line| {
         let line = line.unwrap();
@@ -161,15 +158,32 @@ fn parse(file_path: &str, part_two: bool) -> Result<Problem, ParsingError> {
             let left = captures["left"].to_string();
             let right = captures["right"].to_string();
 
-            let node = TreeNode::find_or_create(&root, &source);
-            let _ = node.set(TreeNode {
-                label: source,
-                left: TreeNode::find_or_create(&root, &left),
-                right: TreeNode::find_or_create(&root, &right),
-            });
+            println!("processing new line {} = {}, {}", &source, &left, &right);
+
+            if source != "ZZZ" { 
+            let node = TreeNode::find_or_create(&root, &source).unwrap();
+
+            let left_child = TreeNode::find_or_create(&root, &left);
+            let right_child = TreeNode::find_or_create(&root, &right);
+
+            let mut node = node.borrow_mut();
+            node.left = left_child;
+            node.right = right_child;
+            }
+                
+            // node.left = Some(Rc::new(RefCell::new(TreeNode {
+            //     label: left.to_owned(),
+            //     left: None,
+            //     right: None,
+            // })));
+            //
+            // node.right = Some(Rc::new(RefCell::new(TreeNode {
+            //     label: right.to_owned(),
+            //     left: None,
+            //     right: None,
+            // })));
         }
     });
-
 
     Ok(Problem { root, instructions })
 }
@@ -177,28 +191,28 @@ fn parse(file_path: &str, part_two: bool) -> Result<Problem, ParsingError> {
 fn process(file_path: &str, part_two: bool) -> io::Result<usize> {
     let problem = parse(file_path, part_two).unwrap();
 
-    dbg!(&problem);
+    // dbg!(&problem);
 
     let mut noderef: NodeRef = problem.root;
     let mut instr_iter = problem.instructions.iter();
     let mut counter: usize = 0;
 
-    loop {
-        let node = noderef.get().unwrap();
-
-        if node.label == "ZZZ" {
-            break;
-        }
-
-        dbg!(&node.label);
-
-        if *(instr_iter.next().unwrap()) == 'R' {
-            noderef = node.left.clone();
-        } else {
-            noderef = node.right.clone();
-        }
-        counter += 1;
-    }
+    // loop {
+    //     let node = noderef.unwrap();
+    //
+    //     if node.borrow().label == "ZZZ" {
+    //         break;
+    //     }
+    //
+    //     dbg!(&node.borrow().label);
+    //
+    //     if *(instr_iter.next().unwrap()) == 'L' {
+    //         noderef = node.borrow().left.clone();
+    //     } else {
+    //         noderef = node.borrow().right.clone();
+    //     }
+    //     counter += 1;
+    // }
 
     Ok(counter)
 }
