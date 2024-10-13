a54d67025899b33090b7c2e3ad09a04509dda2a4
Alek Gajos <alek.gajos@gmail.com>
2024-10-13 17:23:25 +0200

    Day 19 part 2 WIP

--- rust/aoc2023-day19/src/main.rs
+++ rust/aoc2023-day19/src/main.rs
@@ -1,7 +1,7 @@
 use std::collections::HashMap;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
-use std::ops::Index;
+use std::ops::{self, Index};
 
 #[derive(Debug)]
 struct Part {
@@ -30,6 +30,134 @@ enum Rule {
 #[derive(Debug)]
 struct Workflow {
     rules: Vec<Rule>,
+}
+
+#[derive(Debug, Clone)]
+struct Range {
+    low: i64,
+    high: i64,
+    label: String,
+    is_empty: bool,
+}
+
+impl ops::MulAssign<Range> for Range {
+    fn mul_assign(&mut self, rhs: Range) {
+        if self.high < rhs.low || self.low > rhs.high {
+            self.is_empty = true;
+        } else if self.high >= rhs.low {
+            self.low = self.low.max(rhs.low);
+            self.high = self.high.min(rhs.high);
+        }
+    }
+}
+
+impl Range {
+    fn new(label: &str) -> Range {
+        Range {
+            low: Range::LOW_LIMIT,
+            high: Range::HIGH_LIMIT,
+            label: label.to_string(),
+            is_empty: false,
+        }
+    }
+
+    fn count(&self) -> i64 {
+        if !self.is_empty {
+            self.high - self.low + 1
+        } else {
+            0
+        }
+    }
+
+    const LOW_LIMIT: i64 = 1;
+    const HIGH_LIMIT: i64 = 4000;
+}
+
+type Ranges = HashMap<String, Range>;
+
+struct Processor<'a> {
+    workflows: &'a HashMap<String, Workflow>,
+}
+
+impl<'a> Processor<'a> {
+    /// First range in the tuple matches the rule, the second one passes on
+
+    fn dispatch(&self, target: &str, ranges: Ranges) -> i64 {
+        match target {
+            "A" => {
+                ranges.values().map(|r| r.count()).product()
+            },
+            "R" => {
+                0
+            },
+            _ => {
+                let new_workflows = self.workflows.get(target).unwrap().rules.as_slice();
+                self.step(ranges, new_workflows)
+            }
+        }
+        
+    }
+
+    fn step(&self, ranges: Ranges, rules: &[Rule]) -> i64 {
+        match rules {
+            [head, tail @ ..] => {
+                if let Rule::Final(target) = head {
+                    self.dispatch(target, ranges)
+                } else {
+                    let (matches, passes, target) = rule_to_ranges(head);
+                    let mut ranges_left = ranges.clone();
+                    *ranges_left.get_mut(&matches.label).unwrap() *= matches;
+
+                    let mut ranges_right = ranges.clone();
+                    *ranges_right.get_mut(&passes.label).unwrap() *= passes;
+
+                    return self.dispatch(target, ranges_left) + self.step(ranges_right, tail);
+                }
+            }
+            _ => panic!(),
+        }
+    }
+}
+
+fn rule_to_ranges(rule: &Rule) -> (Range, Range, String) {
+    match rule {
+        Rule::Mid(what, thr, is_less_than_thr, target) => {
+            if *is_less_than_thr {
+                (
+                    Range {
+                        is_empty: false,
+                        low: Range::LOW_LIMIT,
+                        high: thr - 1,
+                        label: what.to_string(),
+                    },
+                    Range {
+                        is_empty: false,
+                        low: *thr,
+                        high: Range::HIGH_LIMIT,
+                        label: what.to_string(),
+                    },
+                    target.to_string(),
+                )
+            } else {
+                (
+                    Range {
+                        is_empty: false,
+                        low: thr + 1,
+                        high: Range::HIGH_LIMIT,
+                        label: what.to_string(),
+                    },
+                    Range {
+                        is_empty: false,
+                        low: Range::LOW_LIMIT,
+                        high: *thr,
+                        label: what.to_string(),
+                    },
+                    target.to_string(),
+                )
+            }
+        }
+        Rule::Final(_) => panic!("Final rule not expected here."),
+    }
 }
 
 fn process(file_path: &str, part_two: bool) -> i64 {
@@ -67,10 +195,23 @@ fn process(file_path: &str, part_two: bool) -> i64 {
         }
     });
 
-    items
-        .iter()
-        .map(|item| process_item(&workflows, item))
-        .sum()
+    if !part_two {
+        items
+            .iter()
+            .map(|item| process_item(&workflows, item))
+            .sum()
+    } else {
+        let proc = Processor {
+            workflows: &workflows,
+        };
+
+        let mut full_ranges = HashMap::new();
+        for name in ["x", "m", "a", "s"] {
+            full_ranges.insert(name.to_string(), Range::new(name));
+        }
+
+        proc.step(full_ranges, workflows.get("in").unwrap().rules.as_slice())
+    }
 }
 
 fn process_item(workflows: &HashMap<String, Workflow>, item: &Part) -> i64 {
@@ -142,9 +283,9 @@ fn main() {
     let test_file = "test.txt";
     let input_file = "input.txt";
 
-    println!("{}", process(test_file, false));
-    println!("{}", process(input_file, false));
+    // println!("{}", process(test_file, false));
+    // println!("{}", process(input_file, false));
     //
-    // println!("{}", process(test_file, true));
+    println!("{}", process(test_file, true));
     // println!("{}", process(input_file, true));
 }
